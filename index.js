884bc20e7840e10a598398ef5f766a2a582c5108
noufanelachola <noufansadiqelachola@gmail.com>
2024-01-20 20:04:20 +0530

    second

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const app = express();
+app.get("/", (req, res) => {
+    res.json("it is working");
+});
 
-app.get("/",(req,res) => {res.json("it is working")});
-
-app.listen(process.env.PORT || 3000,() => {
-    console.log(`App is running on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
 });
